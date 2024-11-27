09212371a53dedb1e988100807094b5cfb7365b1
ijazhossain <ijaz.hossain35@gmail.com>
2024-11-27 11:35:19 +0600

    font and image optimization added

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import React from "react";
-
+import image from "../../assets/photo-1732089784928-d1a4cac61912.webp";
 const GalleryPage = () => {
   return (
     <div>
       <h1>Image optimizations</h1>
-      <h2 className="text-center">Regular image</h2>
+      <h2 className="text-center text-4xl my-6">Regular image</h2>
       <img
         src="https://images.unsplash.com/photo-1732089784928-d1a4cac61912?q=80&w=2976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="image"
@@ -13,9 +13,17 @@ const GalleryPage = () => {
         height={300}
         className="mx-auto"
       />
-      <h2 className="text-center">Next image</h2>
+      <h2 className="text-center text-4xl my-6">Next image</h2>
       <Image
         src="https://images.unsplash.com/photo-1732089784928-d1a4cac61912?q=80&w=2976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="image"
+        width={500}
+        height={300}
+        className="mx-auto"
+      />
+      <h2 className="text-center text-4xl my-6">Local image</h2>
+      <Image
+        src={image}
         alt="image"
         width={500}
         height={300}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,11 @@
-import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+import { Inter } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata = {
@@ -20,11 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
